9da59e4d0320599029241f90bff931245aade09f
sid <siddharthingle1010@gmail.com>
2023-09-30 13:02:19 +0530

    commit1

---
+++ BackEndOfShivAssociates/src/main/java/BackEndOfShivAssociates/BackEndOfShivAssociates/BackEndOfShivAssociatesApplication.java
@@ -0,0 +1,13 @@
+package BackEndOfShivAssociates.BackEndOfShivAssociates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndOfShivAssociatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndOfShivAssociatesApplication.class, args);
+	}
+
+}
---
+++ BackEndOfShivAssociates/src/test/java/BackEndOfShivAssociates/BackEndOfShivAssociates/BackEndOfShivAssociatesApplicationTests.java
@@ -0,0 +1,13 @@
+package BackEndOfShivAssociates.BackEndOfShivAssociates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndOfShivAssociatesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
